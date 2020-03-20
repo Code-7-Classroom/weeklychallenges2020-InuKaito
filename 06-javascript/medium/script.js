3b6108f7aeffdc1c557513c1fd12458d31a2abc0
Nathan Cha <nathancha777@gmail.com>
2020-03-20 11:37:53 -0400

    maded chanes to medium challenge using &&

--- 06-javascript/medium/script.js
+++ 06-javascript/medium/script.js
@@ -4,12 +4,13 @@ var talk = prompt('please write in all caps for shouting or write in all lowerca
 var talk2 = ' is whispering';
 var talk3 = ' is shouting';
 var talk4 = ' is talking normal';
-if (talk === talk.toUpperCase(talk3)) {
-    console.log(talk + talk3);
-} 
-else if (talk === talk.toLowerCase(talk2)) {
-    console.log(talk + talk2);
-}  
-else {
-    console.log(talk + talk4);
+if (talk !== talk.toUpperCase() && talk !== talk.toLowerCase()) {
+    // talking normal
+    console.log(talk + talk4)
+}
+else if (talk === talk.toLowerCase()) {
+         console.log(talk + talk2);
+}
+else { (talk === talk.toUpperCase()) 
+     console.log(talk + talk3);
 }
