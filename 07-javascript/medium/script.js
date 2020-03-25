b9ec6e1c56fe03b9926d46e2c64c9db30109bce2
Nathan Cha <nathancha777@gmail.com>
2020-03-25 14:46:28 -0400

    made changes to medium challenge

--- 07-javascript/medium/script.js
+++ 07-javascript/medium/script.js
@@ -2,40 +2,40 @@ var month = parseInt(prompt('Please enter a number in between 1-12'));
 
 switch(month) {
    case 1:
-        alert('Month of ' + month + ' is January');
+      console.log('Month of ' + month + ' is January');
       break;
    case 2:
-        alert('Month of ' + month + ' is February');
+      console.log('Month of ' + month + ' is February');
       break;
    case 3:
-        alert('Month of ' + month + ' is March');
+      console.log('Month of ' + month + ' is March');
       break;
    case 4:
-        alert('Month of ' + month + ' is April');
+      console.log('Month of ' + month + ' is April');
       break;
    case 5:
-        alert('Month of ' + month + ' is May');
+      console.log('Month of ' + month + ' is May');
       break;
    case 6:
-        alert('Month of ' + month + ' is June');
+      console.log('Month of ' + month + ' is June');
       break;
    case 7:
-        alert('Month of ' + month + ' is July');
+      console.log('Month of ' + month + ' is July');
       break;
    case 8:
-        alert('Month of ' + month + ' is August');
+      console.log('Month of ' + month + ' is August');
       break;
    case 9:
-        alert('Month of ' + month + ' is September');
+      console.log('Month of ' + month + ' is September');
       break;
    case 10:
-        alert('Month of ' + month + ' is October');
+      console.log('Month of ' + month + ' is October');
       break;
    case 11:
-        alert('Month of ' + month + ' is November');
+      console.log('Month of ' + month + ' is November');
       break;
    case 12:
-        alert('Month of ' + month + ' is December');
+      console.log('Month of ' + month + ' is December');
       break;
    default:
         alert('You have entered an invalid number');
