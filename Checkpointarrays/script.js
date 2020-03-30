69f909b851f87d3bb7d2a8fbf32cc29d07b48561
Nathan Cha <nathancha777@gmail.com>
2020-03-30 11:12:56 -0400

    checkpoint arrays folder

---
+++ Checkpointarrays/script.js
@@ -0,0 +1,11 @@
+// Name 3 javascript array methods
+// .join()
+// .indexOf()
+// .push
+
+var drinks =[];
+drinks=['water', 'coffee', 'tea'];
+var foods =[];
+foods = ['apple', 'sandwich', 'chips'];
+var meals =[drinks, foods];
+console.log(meals);
