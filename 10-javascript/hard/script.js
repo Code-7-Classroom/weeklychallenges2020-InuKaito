5a00bc367d433677a89f3fcafa3177fee8aba2da
Nathan Cha <nathancha777@gmail.com>
2020-04-16 15:41:47 -0400

    made more changes to hard challenge

--- 10-javascript/hard/script.js
+++ 10-javascript/hard/script.js
@@ -15,15 +15,18 @@
 const red = document.querySelector('.red');
 const white = document.querySelector('.white');
 const body = document.querySelector('.body');
+const changeBackColor = () => {
 
-red.addEventListener('click', () => {
-    body.style['background-color'] = 'red';
-    red.style['background-color'] = 'red';
-    red.style['color'] = 'white';
-});
+    red.addEventListener('click', () => {
+        body.style['background-color'] = 'red';
+        red.style['background-color'] = 'red';
+        red.style['color'] = 'white';
+        });
 
-white.addEventListener('click', () => {
-    body.style['background-color'] = 'white';
-    white.style['background-color'] = 'white';
-    white.style['color'] = 'red';
-})
+    white.addEventListener('click', () => {
+        body.style['background-color'] = 'white';
+        white.style['background-color'] = 'white';
+        white.style['color'] = 'red';
+        });
+};
+changeBackColor();
