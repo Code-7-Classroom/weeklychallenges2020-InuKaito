1a50d980a0b9939bc37d37608d9ea6f3613a2aa9
Nathan Cha <nathancha777@gmail.com>
2020-04-16 15:02:38 -0400

    made changes to hard challenge

--- 10-javascript/hard/script.js
+++ 10-javascript/hard/script.js
@@ -1,21 +1,29 @@
 // document.querySelector(".red").addEventListener('click', () => {
 //     document.querySelector(".red").style['background-color'] = 'red';
+//     document.querySelector(".red").style['color'] = 'white';
+//     document.querySelector(".body").style['background-color'] = 'red';
+
 //   });
 
 //   document.querySelector(".white").addEventListener('click', () => {
 //     document.querySelector(".white").style['background-color'] = 'white';
+//     document.querySelector(".body").style['background-color'] = 'white';
+//     document.querySelector(".white").style['color'] = 'red';
 //   });
 
 
-  const red = document.querySelector(".red");
-  const white = document.querySelector(".white");
+const red = document.querySelector('.red');
+const white = document.querySelector('.white');
+const body = document.querySelector('.body');
 
-  red.addEventListener('click', () => {
-      red.style['background-color'] = 'red';
-      red.style['color'] = 'white';
-  });
+red.addEventListener('click', () => {
+    body.style['background-color'] = 'red';
+    red.style['background-color'] = 'red';
+    red.style['color'] = 'white';
+});
 
-  white.addEventListener('click', () => {
-      white.style['background-color'] = 'white';
-      white.style['color'] = 'red';
-  })
+white.addEventListener('click', () => {
+    body.style['background-color'] = 'white';
+    white.style['background-color'] = 'white';
+    white.style['color'] = 'red';
+})
